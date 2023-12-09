a83210b0bd16f4702e1d40506add6190b98c3331
Karol Kowalczyk <KarolKowalczykPL@Gmail.com>
2023-12-10 00:17:58 +0100

    join logo responsive

--- assets/script/script.js
+++ assets/script/script.js
@@ -90,19 +90,25 @@ setTimeout(function init() {
         let header = document.getElementById('main-header');
         let midSection = document.getElementById('mid-section');
         let complianceLinks = document.getElementById('compliance-links-div');
+        let responsiveSignUp = document.getElementById('responsive-sign-up');
+        let content = document.getElementById('content');
 
-        header.classList.remove('opacity-zero')
-        midSection.classList.remove('opacity-zero')
-        complianceLinks.classList.remove('opacity-zero')
+        header.classList.remove('opacity-zero');
+        midSection.classList.remove('opacity-zero');
+        complianceLinks.classList.remove('opacity-zero');
+        responsiveSignUp.classList.remove('opacity-zero');
 
         header.classList.add('opacity-one');
         midSection.classList.add('opacity-one');
         complianceLinks.classList.add('opacity-one');
-
+        responsiveSignUp.classList.add('opacity-one');
+        content.classList.add('new-color');
+        content.classList.remove('dark');
 
         joinLogo.classList.add('transition');
 
         removeJoinClass();
+        
 }, 1000)
 
 function removeJoinClass() {
