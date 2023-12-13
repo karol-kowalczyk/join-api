9b67db43978abbc1ea4d27237dd350b8a4867e65
Karol Kowalczyk <KarolKowalczykPL@Gmail.com>
2023-12-13 22:22:56 +0100

    added templates into board.html

---
+++ assets/script/template.js
@@ -0,0 +1,16 @@
+async function includeHTML() {
+        let includeElements = document.querySelectorAll('[w3-include-html]');
+
+        for (let i = 0; i < includeElements.length; i++) {
+                const element = includeElements[i];
+                
+                let file = element.getAttribute('w3-include-html');
+                resp = await fetch(file);
+
+                if(resp.ok) {
+                        element.innerHTML = await resp.text();
+                } else {
+                        element.innerHTML = 'Page not found';
+                }
+        }
+}
