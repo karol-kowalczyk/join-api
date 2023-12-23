65e611ad2f3c83f4817a83059d18cc218ef44a9a
Karol Kowalczyk <KarolKowalczykPL@Gmail.com>
2023-12-23 19:12:36 +0100

    register.js commitet

--- assets/script/register.js
+++ assets/script/register.js
@@ -4,11 +4,32 @@ let userName = document.getElementById('username');
 let eMail = document.getElementById('email');
 let setPassword = document.getElementById('password-field');
 let passwordConfirm = document.getElementById('password-field-confirm');
-let content = document.getElementById('content');
+let signedUpDiv = document.getElementById('sucessfully-signed-up-div');
+let midSection = document.getElementById('mid-section');
+let logo = document.getElementById('logo');
+let body = document.body;
+let links = document.getElementById('compliance-links-div');
+let midSec = document.getElementById('mid-section');
 
+/**
+ * This function is used to check if the passwords in the password fields are similar.
+ * 
+ * 
+ */
+function checkPassword() {
+        return setPassword.value === passwordConfirm.value;
+}
+
+let passwordMatch = checkPassword();
+
+/**
+ * This async function checks if the entered passwords match, and if so, creates a new user object
+ *  with provided name, email, and password, adds it to a users array, triggers a successful sign-up notification, 
+ *  and redirects to the index HTML page after a 5-second delay; otherwise, it alerts that the passwords do not match.
+ * 
+ * 
+ */
 async function register() {
-        let passwordMatch = checkPassword();
-
         if (passwordMatch) {
                 let newUser = {
                         'name': userName.value,
@@ -18,24 +39,49 @@ async function register() {
 
                 users.push(newUser);
                 await signedUpSuccesfully();
+                resetForm();
                 setTimeout(() => {
-                        window.location = '/join-project-management-tool/index.html';
+                        openIndexHTML();
                 }, 5000);
         } else {
-                alert('Pawword does not match.');
+                alert('Password do not match.');
         }
-
-
 }
 
-function checkPassword() {
-        return setPassword.value === passwordConfirm.value;
+/**
+ * This function reset the form element.
+ * 
+ * 
+ */
+function resetForm() {
+        userName.innerHTML = '';
+        eMail.innerHTML = '';
+        setPassword.innerHTML = '';
+        passwordConfirm.innerHTML = '';
 }
 
+/**
+ * This async function modifies the CSS classes of various HTML elements, making the sign-up notification
+ *  div visible and applying opacity and display changes to other elements, effectively creating
+ *  a visualtransition to indicate a successful sign-up.
+ * 
+ * 
+ */
 async function signedUpSuccesfully() {
-        content.innerHTML = /*html*/`
-        <div id="sucessfully-signed-up-div">
-        <btn id="sucessfully-signed-up">You Signed Up successfully</btn>
-        </div>
-        `;
+        signedUpDiv.classList.remove('d-none');
+        signedUpDiv.classList.add('opacityOne');
+        midSection.classList.add('opacityZero');
+        logo.classList.add('d-none');
+        body.classList.add('bg-grey');
+        links.classList.add('d-none');
+        midSec.classList.add('d-none');
 }
+
+/**
+ * This function redirects to the login page.
+ * 
+ */
+function openIndexHTML() {
+        window.location = '/join-project-management-tool/index.html';
+}
+
--- assets/script/storage.js
+++ assets/script/storage.js
@@ -1,12 +1,22 @@
 const STORAGE_TOKEN = '7I3GVB4FSQIZUZFO7U18ACMHMX7UZAKX2PIW985';
 const STORAGE_URL = 'https://remote-storage.developerakademie.org/item';
 
-
+/**
+ * This function send users information to the local storage.
+ * @param {string} key - The key of the item to be set like e.g., name, username, email etc.
+ * @param {any} value - This is the value of the key.
+ * @returns {Promise} -  A Promise that resolves with the result of the storage operation.
+ */
 async function setItem(key, value) {
         const payload = {key, value, token:STORAGE_TOKEN};
         return fetch(STORAGE_URL, {method: 'POST', body: JSON.stringify(payload)}).then(res => res.json()); 
 }
 
+/**
+ * Retrieves information from local storage based on the provided key.
+ * @param {string} key - The key for which information was previously stored by the user.
+ * @returns {Promise} - A Promise that resolves with the retrieved information.
+ */
 async function getItem(key) {
         const url = `${STORAGE_URL}?key=${key}&token=${STORAGE_TOKEN}`;
         return fetch(url).then(res => res.json());
