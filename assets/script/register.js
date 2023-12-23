ff1d1c892d2e341d6c4d1bb508aa3fc726b5f5d6
Karol Kowalczyk <KarolKowalczykPL@Gmail.com>
2023-12-23 14:51:35 +0100

    remote storage for signUp.html and alert for successfully signed in and failed sign up

--- assets/script/register.js
+++ assets/script/register.js
@@ -1,29 +1,41 @@
 let users = [];
 
-async function registerBtn(){
-        loadUsers();
-}
-
-async function loadUsers() {
-        try {
-                users = JSON.parse(await getItem('users'));
-        } catch(e) {
-                console.info('could not load users');
-        }
-}
+let userName = document.getElementById('username');
+let eMail = document.getElementById('email');
+let setPassword = document.getElementById('password-field');
+let passwordConfirm = document.getElementById('password-field-confirm');
+let content = document.getElementById('content');
 
 async function register() {
-        registerBtn.disabled = true;
-        users.push({
-                email: email.value,
-                password: password.value,
-        });
-        await setItem('users', JSON.stringify(users));
-        resetForm();
+        let passwordMatch = checkPassword();
+
+        if (passwordMatch) {
+                let newUser = {
+                        'name': userName.value,
+                        'email': eMail.value,
+                        'password': setPassword.value,
+                }
+
+                users.push(newUser);
+                await signedUpSuccesfully();
+                setTimeout(() => {
+                        window.location = '/join-project-management-tool/index.html';
+                }, 5000);
+        } else {
+                alert('Pawword does not match.');
+        }
+
+
 }
 
-function resetForm() {
-        email.value = '';
-        password.value = '';
-        registerBtn.disabled = false;
+function checkPassword() {
+        return setPassword.value === passwordConfirm.value;
+}
+
+async function signedUpSuccesfully() {
+        content.innerHTML = /*html*/`
+        <div id="sucessfully-signed-up-div">
+        <btn id="sucessfully-signed-up">You Signed Up successfully</btn>
+        </div>
+        `;
 }
