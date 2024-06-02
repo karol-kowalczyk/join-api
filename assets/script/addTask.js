7f68e97fb76221ba1158682268a5ea2166fa0b48
Karol Kowalczyk <KarolKowalczykPL@Gmail.com>
2024-06-02 13:40:32 +0200

    fixed the position of the message div in the add_task.html

--- assets/script/addTask.js
+++ assets/script/addTask.js
@@ -25,32 +25,74 @@ function toggleButtonState(buttonId) {
         break;
     }
   } else {
-    button.style.backgroundColor = ""; 
+    button.style.backgroundColor = "";
   }
 }
 
 function clearAllEntries() {
-    const titleInput = document.querySelector('.input-fields[type="text"]');
-    const descriptionTextarea = document.querySelector('textarea');
-    const assignedToInput = document.getElementById('contacts-dropdown');
-    const dueDateInput = document.getElementById('date');
-    const prioButtons = document.querySelectorAll('.prio-button');
-    const addSubtask = document.getElementById('addSubtask');
-    deleteAllInputFields(titleInput, descriptionTextarea, assignedToInput, dueDateInput, addSubtask);
-    resetAllBtns(prioButtons);
+  const titleInput = document.querySelector('.input-fields[type="text"]');
+  const descriptionTextarea = document.querySelector("textarea");
+  const assignedToInput = document.getElementById("contacts-dropdown");
+  const dueDateInput = document.getElementById("date");
+  const prioButtons = document.querySelectorAll(".prio-button");
+  const addSubtask = document.getElementById("addSubtask");
+  deleteAllInputFields(
+    titleInput,
+    descriptionTextarea,
+    assignedToInput,
+    dueDateInput,
+    addSubtask
+  );
+  resetAllBtns(prioButtons);
 }
 
 function resetAllBtns(prioButtons) {
-    prioButtons.forEach(button => {
-        button.classList.remove('active');
-        button.style.backgroundColor = '';
-    });
+  prioButtons.forEach((button) => {
+    button.classList.remove("active");
+    button.style.backgroundColor = "";
+  });
 }
 
-function deleteAllInputFields(titleInput, descriptionTextarea, assignedToInput, dueDateInput, addSubtask) {
-    titleInput.value = '';
-    descriptionTextarea.value = '';
-    assignedToInput.value = '';
-    dueDateInput.value = '';
-    addSubtask.value = '';
+function deleteAllInputFields(
+  titleInput,
+  descriptionTextarea,
+  assignedToInput,
+  dueDateInput,
+  addSubtask
+) {
+  titleInput.value = "";
+  descriptionTextarea.value = "";
+  assignedToInput.value = "";
+  dueDateInput.value = "";
+  addSubtask.value = "";
 }
+
+function showMessage(isEmpty) {
+    let messageDiv = document.getElementById('signUpMessage');
+    if (isEmpty) {
+      messageDiv.textContent = 'Please fill in all required fields.';
+      messageDiv.style.position = 'fixed';
+    } else {
+      messageDiv.textContent = 'Task is successfully created.';
+      messageDiv.style.position = 'fixed';
+    }
+    messageDiv.style.opacity = 1;
+    setTimeout(() => {
+      messageDiv.style.opacity = 0;
+    }, 2100);
+  }
+  
+  function checkRequiredFields() {
+    const requiredFields = document.querySelectorAll(".required");
+    let isEmpty = false;
+  
+    requiredFields.forEach((field) => {
+      if (field.value === "") {
+        isEmpty = true;
+      }
+    });
+  
+    showMessage(isEmpty);
+  }
+  
+
