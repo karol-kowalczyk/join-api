ced1204803d3db14a4003ec26f4f604b16fc2f42
Karol Kowalczyk <KarolKowalczykPL@Gmail.com>
2024-06-02 13:11:13 +0200

    I have created three functions: "resetAllBtns" to remove the active state and background color from priority buttons, "deleteAllInputFields" to clear the values of various input fields, and "clearAllEntries" to coordinate the clearing of input fields and resetting of buttons when invoke"

--- assets/script/addTask.js
+++ assets/script/addTask.js
@@ -1,19 +1,15 @@
 function toggleButtonState(buttonId) {
   const button = document.getElementById(buttonId);
 
-  // Deaktiviere alle anderen Prio-Buttons
   const buttons = document.querySelectorAll(".prio-button");
   buttons.forEach((btn) => {
     if (btn.id !== buttonId) {
       btn.classList.remove("active");
-      btn.style.backgroundColor = ""; // Zurücksetzen auf Standard-Hintergrundfarbe
+      btn.style.backgroundColor = "";
     }
   });
 
-  // Toggle den angeklickten Prio-Button
   const isActive = button.classList.toggle("active");
-
-  // Setze die Hintergrundfarbe basierend auf dem angeklickten Zustand
   if (isActive) {
     switch (buttonId) {
       case "urgent":
@@ -29,6 +25,32 @@ function toggleButtonState(buttonId) {
         break;
     }
   } else {
-    button.style.backgroundColor = ""; // Zurücksetzen auf Standard-Hintergrundfarbe
+    button.style.backgroundColor = ""; 
   }
+}
+
+function clearAllEntries() {
+    const titleInput = document.querySelector('.input-fields[type="text"]');
+    const descriptionTextarea = document.querySelector('textarea');
+    const assignedToInput = document.getElementById('contacts-dropdown');
+    const dueDateInput = document.getElementById('date');
+    const prioButtons = document.querySelectorAll('.prio-button');
+    const addSubtask = document.getElementById('addSubtask');
+    deleteAllInputFields(titleInput, descriptionTextarea, assignedToInput, dueDateInput, addSubtask);
+    resetAllBtns(prioButtons);
+}
+
+function resetAllBtns(prioButtons) {
+    prioButtons.forEach(button => {
+        button.classList.remove('active');
+        button.style.backgroundColor = '';
+    });
+}
+
+function deleteAllInputFields(titleInput, descriptionTextarea, assignedToInput, dueDateInput, addSubtask) {
+    titleInput.value = '';
+    descriptionTextarea.value = '';
+    assignedToInput.value = '';
+    dueDateInput.value = '';
+    addSubtask.value = '';
 }
