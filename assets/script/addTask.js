1cefb7783fdfefc038947010d7ceac92894666f5
Karol Kowalczyk <KarolKowalczykPL@Gmail.com>
2024-06-02 12:51:24 +0200

    changed the colors of the prio buttons

--- assets/script/addTask.js
+++ assets/script/addTask.js
@@ -1,34 +1,34 @@
-// JavaScript
-
-// Funktionen zum Behalten des Status der Prio-Buttons
 function toggleButtonState(buttonId) {
-    const button = document.getElementById(buttonId);
-    const buttons = document.querySelectorAll('.prio-button');
-    
-    buttons.forEach(btn => {
-        if (btn.id !== buttonId) {
-            btn.classList.remove('active');
-            btn.style.backgroundColor = '';
-        }
-    });
+  const button = document.getElementById(buttonId);
 
-    const isActive = button.classList.toggle('active');
-
-    if (isActive) {
-        switch (buttonId) {
-            case 'urgent':
-                button.style.backgroundColor = '#FF4910';
-                break;
-            case 'medium':
-                button.style.backgroundColor = '#FFA500';
-                break;
-            case 'low':
-                button.style.backgroundColor = '#7AE229';
-                break;
-            default:
-                break;
-        }
-    } else {
-        button.style.backgroundColor = '';
+  // Deaktiviere alle anderen Prio-Buttons
+  const buttons = document.querySelectorAll(".prio-button");
+  buttons.forEach((btn) => {
+    if (btn.id !== buttonId) {
+      btn.classList.remove("active");
+      btn.style.backgroundColor = ""; // Zurücksetzen auf Standard-Hintergrundfarbe
     }
+  });
+
+  // Toggle den angeklickten Prio-Button
+  const isActive = button.classList.toggle("active");
+
+  // Setze die Hintergrundfarbe basierend auf dem angeklickten Zustand
+  if (isActive) {
+    switch (buttonId) {
+      case "urgent":
+        button.style.backgroundColor = "#FF8010"; // Orange
+        break;
+      case "medium":
+        button.style.backgroundColor = "#FFD900"; // Gelb
+        break;
+      case "low":
+        button.style.backgroundColor = "#29E267"; // Grün
+        break;
+      default:
+        break;
+    }
+  } else {
+    button.style.backgroundColor = ""; // Zurücksetzen auf Standard-Hintergrundfarbe
+  }
 }
