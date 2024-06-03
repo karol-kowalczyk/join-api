6cf2b90cc2d8ac161047659e9d1f15372a06e23f
Karol Kowalczyk <KarolKowalczykPL@Gmail.com>
2024-06-03 05:08:05 +0200

    created a popUpdiv where I can view and modify all the information contained in the card

--- assets/script/board.js
+++ assets/script/board.js
@@ -89,7 +89,7 @@ function showTaskInBoard() {
         `#${targetBoardId} .task-container`
     );
 
-    taskContainer.innerHTML += `<div class="task-div">${targetBoardId}</div>`;
+    taskContainer.innerHTML += /*html*/ `<div class="task-div">${targetBoardId}</div>`;
     updateTaskVisibility();
     closeDetails();
 }
@@ -163,4 +163,71 @@ function clearAllEntries() {
         document.querySelectorAll(".prio-button")
     );
     resetAllBtns(document.querySelectorAll(".prio-button"));
+}
+
+
+
+function showTaskInformation() {
+    document.body.style.overflow = "hidden";
+    let taskDiv = document.getElementById('taskDiv');
+    let title = taskDiv.textContent; // Korrektur hier: textContent statt value
+    taskDiv.classList.add('no-hover'); // Füge die Klasse hinzu, um den Hover-Effekt zu deaktivieren
+    console.log(title);
+    taskDiv.innerHTML = /*HTML*/ `<div id="taskInformationPopup" class="" >  
+    <div class="popUp" onclick="stopPropagationFunction(event)">
+    <div id="content-popUp">
+        <div class="left-side">
+            <div id="signUpMessage" class="message-div"></div>
+            <div class="title">Title <div class="red-star">*</div>
+            </div>
+            <input id="inputField" class="input-fields required" type="text" placeholder="Enter a title" value="${title}">
+
+            <div class="title">Description</div>
+
+            <textarea rows="4" cols="50" name="description" placeholder="Enter a Description"></textarea>
+
+            <div class="title">Assigned to</div>
+            <input id="contacts-dropdown" class="input-fields" type="text" placeholder="Select contacts to assign">
+            <div id="required-div">
+                <div class="red-star">*</div>This field is required
+            </div>
+        </div>
+        <div id="border"></div>
+        <div class="right-side">
+            <div class="date required">Due date <div class="red-star">*</div>
+            </div>
+            <input id="date" type="date">
+
+            <div class="title">Prio</div>
+            <div id="prioritize">
+                <button id="urgent" class="prio-button" onclick="toggleButtonState('urgent')">Urgent <img class="prio-icons" src="./assets/img/add_task/urgent.svg" alt="urgent"></button>
+                <button id="medium" class="prio-button" onclick="toggleButtonState('medium')">Medium <img class="prio-icons" src="./assets/img/add_task/medium.svg" alt="medium"></button>
+                <button id="low" class="prio-button" onclick="toggleButtonState('low')">Low <img class="prio-icons" src="./assets/img/add_task/low.svg" alt="low"></button>
+                
+            </div>
+            <div class="title">Category</div>
+            <input id="tasks-dropdown" class="input-fields" type="text" placeholder="Select task category">
+            <div class="title">Subtasks</div>
+            <div id="subtask-div">
+                <input id="addSubtask" class="input-fields" type="text" placeholder="Add new subtask">
+                <div id="create-delete-div">
+                    <div id="responsive-required-div" class="d-none">
+                        <div class="red-star">*</div>This field is required
+                    </div>
+                    <div id="responsive-clear-create-btns">
+                        <button onclick="clearAllEntries()" id="clear-btn">Clear <img src="./assets/img/add_task/x.svg" alt="delete"></button>
+                        <button onclick="checkAllRequiredFields()" id="create-btn">Save Task <img src="./assets/img/add_task/check.svg" alt="check"></button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</div>`;
+}
+
+function closeInformationTaskDetails() {
+    let taskInformationPopup = document.getElementById('taskInformationPopup');
+    taskInformationPopup.classList.add('d-none');
+    document.body.style.overflow = "auto";
 }
