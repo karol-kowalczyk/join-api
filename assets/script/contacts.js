d0e903adb7e20557d8ce64df9417c06ba784f928
Karol Kowalczyk <KarolKowalczykPL@Gmail.com>
2024-06-08 20:25:05 +0200

    test

--- assets/script/contacts.js
+++ assets/script/contacts.js
@@ -1,4 +1,4 @@
 function showContactInformation() {
     let contactInformationDisplay = document.getElementById('contactInformationDisplay');
-    contactInformationDisplay.style.right = '50%';
+    contactInformationDisplay.style.right = '30%';
 }
