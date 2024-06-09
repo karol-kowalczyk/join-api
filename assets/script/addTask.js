9481e4951b0e1c2f356d9d0afb0711f3d5281cc6
Karol Kowalczyk <KarolKowalczykPL@Gmail.com>
2024-06-09 19:53:57 +0200

    added a dropdown menu arrow to assign somebody to a new created task. Still in progress

--- assets/script/addTask.js
+++ assets/script/addTask.js
@@ -112,3 +112,20 @@ function checkRequiredFields() {
 
   showMessage(isEmpty);
 }
+
+
+function openContactDropdownMenu() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+  if (!event.target.matches('#contactsDropdown') && !event.target.matches('.dropdown-arrow')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    for (var i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
