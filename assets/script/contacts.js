a44a999767359d5394fc7a8233dd01ef46c8b3cb
Karol Kowalczyk <KarolKowalczykPL@Gmail.com>
2024-06-17 04:54:44 +0200

    added a thinner margin for the main content div

--- assets/script/contacts.js
+++ assets/script/contacts.js
@@ -1,13 +1,18 @@
 
 async function fetchContacts() {
-    try {
-      const response = await fetch('/path-to-contacts-data'); // Adjust the URL
-      const contacts = await response.json();
-      populateContactsDropdown(contacts);
-    } catch (error) {
-      console.error('Error fetching contacts:', error);
-    }
+  try {
+    const response = await fetch('/path-to-contacts-data'); // Adjust the URL
+    const contacts = await response.json();
+    populateContactsDropdown(contacts);
+  } catch (error) {
+    console.error('Error fetching contacts:', error);
   }
-  
-  document.addEventListener('DOMContentLoaded', fetchContacts);
-  
+}
+
+
+document.addEventListener('DOMContentLoaded', fetchContacts);
+
+
+function addNewContact() {
+
+}
